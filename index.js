9b9f49fef3aa6f77864a8dc82cbeac4d74c7965c
YanethM <yaneth.mejiar@autonoma.edu.co>
2024-03-14 17:16:58 -0500

    Mongo database connection

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // Importación del framework express para realizar solicitudes http
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express();
 
 // Poder acceder a leer archivo .env
 require("dotenv").config();
 const CONNECTION_PORT = process.env.PORT || 3005;
 
-
-
 app.listen(CONNECTION_PORT, () => {
   console.log(`Using the port ${CONNECTION_PORT}`);
 });
+
+// Conexión a la base de datos
+const mongo_connect = () => {
+  try {
+    mongoose
+      .connect(process.env.DATABASE_CONNECTION_STRING)
+      .then(() => {
+        console.log("Connected to the database");
+      })
+      .catch((err) => {
+        console.log("Error connecting to the database");
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+mongo_connect();
