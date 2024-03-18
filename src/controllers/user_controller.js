a452e7c581c13234f277a95670e1cade234404d7
YanethM <yaneth.mejiar@autonoma.edu.co>
2024-03-18 09:41:37 -0500

    Add the others controller methods

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -11,26 +11,79 @@ const createUser = async (req, res) => {
       password,
       user_active,
     } = req.body;
-    console.log(req.body);
-  } catch (error) {}
-};
 
+    const newUser = await userModel.create({
+      user_name,
+      lastname,
+      nacionality,
+      document,
+      user_email,
+      password,
+      user_active,
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const listUsers = async (req, res) => {
   try {
-  } catch (error) {}
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
+
 const getUser = async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    console.log(id);
+    const user = await userModel.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
+
 const editUser = async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    const {
+      user_name,
+      lastname,
+      nacionality,
+      document,
+      user_email,
+      password,
+      user_active,
+    } = req.body;
+
+    const user = await userModel.findByIdAndUpdate(id, {
+      user_name,
+      lastname,
+      nacionality,
+      document,
+      user_email,
+      password,
+      user_active,
+    });
+    console.log(user);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
+
 const deleteUser = async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+    await userModel.findByIdAndDelete(id);
+    res.status(200).json({ message: "User deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 
 // http://localhost:3000/api/v1/users/new-user
 router.post("/new-user", userController.createUser);
+router.get("/", userController.listUsers);
+router.get("/:id", userController.getUser);
+router.patch("/edit/:id", userController.editUser);
+router.delete("/delete/:id", userController.deleteUser);
 
 module.exports = router;
